49c6051d1a633756d3f999eb01deeb9aa4a35206
basic-bhavya <bjain.nd@gmail.com>
2021-03-23 01:13:05 +0530

    camera layout + snapshot working

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-
+import OcrIcon from "../assets/ocr-icon";
 
 const Camera = (props) => {
   const canvasRef = useRef();
@@ -14,35 +14,38 @@ const Camera = (props) => {
 
   const takeSnapshot = (canvas, context, video, photo) => {
     setFirstImage(true);
-    resizeCanvas(canvas);
+    resizeCanvas(canvas, video);
     if (video)
-      context.drawImage(video, 0, 0, video.offsetWidth, video.offsetHeight);
+      context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
     if (photo) photo.setAttribute("src", canvas.toDataURL("image/png"));
   };
 
-  useEffect(() => {
-    setDims({
-      width: window.innerWidth,
-      height: window.innerHeight,
-    });
-    const canvas = canvasRef.current;
-    const video = videoRef.current;
-    const photo = photoRef.current;
-    const context = canvas.getContext("2d");
-
-    navigator.getUserMedia(
-      {
+  async function getMedia() {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
         video: {
           facingMode: "environment",
         },
         audio: false,
-      },
-      (stream) => {
-        const mediaStream = new MediaStream(stream);
-        video.srcObject = mediaStream;
-      },
-      (e) => console.log(e)
-    );
+      });
+
+      video.srcObject = stream;
+    } catch (err) {
+      console.log("Stream access error: ", err);
+    }
+  }
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const video = videoRef.current;
+    const photo = photoRef.current;
+    const context = canvas.getContext("2d");
+    setDims({
+      width: video.videoWidth,
+      height: video.videoHeight,
+    });
+
+    getMedia();
 
     const render = () => {
       takeSnapshot(canvas, context, video, photo);
@@ -50,9 +53,9 @@ const Camera = (props) => {
     render();
   }, [trigger]);
 
-  const resizeCanvas = (canvas) => {
-    canvas.width = dims.width;
-    canvas.height = dims.height;
+  const resizeCanvas = (canvas, video) => {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
   };
 
   return (
@@ -65,25 +68,31 @@ const Camera = (props) => {
           autoPlay
           style={{ display: "inline-block", verticalAlign: "top" }}
         ></video>
-        <div className="w-full flex justify-center">
+        <div className="w-full flex justify-between items-center mt-5 h-20 px-2">
+          {firstImage && (
+            <OcrIcon width={dims.width / 8} height={dims.height / 8} />
+          )}
           <button
             id="capture"
-            className="rounded-full w-16 h-16 border-white border-2 my-5"
+            className="absolute left-0 right-0 mx-auto rounded-full w-16 h-16 border-black border-8 text-white bg-white p-2"
+            style={{
+              boxShadow: "0 0 0 2px white",
+            }}
             onClick={() => {
               setTrigger((trigger) => !trigger);
             }}
-          >
-            CAPTURE
-          </button>
+          ></button>
+          {firstImage && (
+            <img
+              className="h-full w-auto"
+              // style={{
+              //   width: dims.width / 6,
+              //   height: dims.height / 6,
+              // }}
+              ref={photoRef}
+            />
+          )}
         </div>
-        {firstImage && (
-          <img
-            id="output"
-            className="mb-5"
-            ref={photoRef}
-            style={{ display: "inline-block", verticalAlign: "top" }}
-          ></img>
-        )}
         <canvas
           id="canvas"
           ref={canvasRef}
