80d103c36bfc137d007e79746411ebd3f7b663e0
basic-bhavya <bjain.nd@gmail.com>
2021-05-20 23:09:22 +0530

    background adjustments

--- components/background.js
+++ components/background.js
@@ -1,13 +1,27 @@
+import {
+  motion,
+  useElementScroll,
+  useTransform,
+  useViewportScroll,
+} from "framer-motion";
 import CircleElement from "../assets/circle-element";
 
 const Background = () => {
+  const { scrollXProgress } = useViewportScroll();
+  const x = useTransform(scrollXProgress, [0, 1], [0, 100]);
+
   return (
     <>
-      <div className="absolute h-[100vh] w-screen overflow-hidden">
+      <motion.div
+        style={{
+          y: x,
+        }}
+        className="absolute h-[100vh] w-screen overflow-hidden"
+      >
         <CircleElement dim={180} y={-10} x={60} del={1} />
         <CircleElement dim={80} y={32} x={-5} pad={12} del={4} />
         <CircleElement dim={120} y={76} x={60} pad={12} del={7} />
-      </div>
+      </motion.div>
       <div className="absolute top-[100vh] h-[200vh] w-screen overflow-hidden">
         <CircleElement dim={180} y={9} x={60} del={3} />
         <CircleElement dim={180} y={38} x={2} del={9} />
