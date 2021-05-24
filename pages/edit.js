923d91351b609032bcfbb93f83f2ba4ca1b5572e
basic-bhavya <bjain.nd@gmail.com>
2021-05-24 21:29:32 +0530

    removed edit

--- pages/edit.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import EditComponent from "../components/EditComponent";
-
-function edit() {
-    return <EditComponent />;
-  // return <div>Nothing to see here</div>;
-}
-
-export default edit;
